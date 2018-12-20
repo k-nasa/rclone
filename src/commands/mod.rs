085266e145af1ae47060c5b1a39585059f4aa2bb
k-nasa <htilcs1115@gmail.com>
2018-12-20 12:26:11 +0900

    Add main implement

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,22 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, App};
+mod cmd_cd;
+mod cmd_clone;
+mod cmd_config;
+
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
 
 pub fn run() {
     let matches = build_app().get_matches();
+
+    if let Some(_) = matches.value_of("repo") {
+        cmd_clone::run(&matches);
+        return;
+    }
+
+    match matches.subcommand() {
+        ("cd", Some(_)) => cmd_cd::run(),
+        ("config", Some(_)) => cmd_config::run(),
+        _ => build_app().print_help().expect("faild print help"),
+    }
 }
 
 fn build_app() -> App<'static, 'static> {
@@ -9,4 +24,8 @@ fn build_app() -> App<'static, 'static> {
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
+        .arg(Arg::with_name("repo").help("git repository name"))
+        .arg(Arg::with_name("ssh").short("s").long("ssh").help("use ssh"))
+        .subcommand(SubCommand::with_name("cd").about("go to clone directory"))
+        .subcommand(SubCommand::with_name("config").about("open config file"))
 }
