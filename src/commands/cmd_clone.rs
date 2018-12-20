9e8be48663f492a2e6b421729bd616a575989f56
k-nasa <htilcs1115@gmail.com>
2018-12-20 16:49:02 +0900

    Add cmd_clone base code

--- src/commands/cmd_clone.rs
+++ src/commands/cmd_clone.rs
@@ -1,3 +1,20 @@
+use crate::config::Config;
 use clap::ArgMatches;
+use failure::{Error, Fail};
+use regex::Regex;
+use std::fs::DirBuilder;
+use std::process::Command;
 
-pub fn run(matches: &ArgMatches) {}
+pub fn run(matches: &ArgMatches) -> Result<(), Error> {
+    let repo = matches.value_of("repo").unwrap();
+    let url = make_repository_url(repo)?;
+
+    let config = Config::load_config()?;
+    let dir_path = config.dir.unwrap();
+
+    create_repos_dir(&dir_path)?;
+
+    fetch_repo(&url, &dir_path)?;
+
+    Ok(())
+}
